871f40880c6330cb03912535366b70a835786a5c
GRGRV9 <niamod@mail.ru>
2021-03-24 12:32:30 +0900

    Achievment class fixed

---
+++ LevelUP App/Assets/Scripts/Achievment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    [CreateAssetMenu(fileName = "Achievment", menuName = "New Achievment")]
+    public class Achievment : ScriptableObject
+    {
+        public string title;
+
+        public string description;
+
+        public enum sphere {Work, Family, Sport};
+
+        public string category;
+
+        public int experience;
+
+        public bool status;
+
+    }
+
